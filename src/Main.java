79458d478f8acd68a23a9eb19fe988c0907d5eaf
Tatsunori <tatsunoripai@gmail.com>
2018-05-14 17:14:24 -0300

    all TSVs

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,24 @@
 import model.Bolsa;
 
 import java.io.IOException;
+import java.text.ParseException;
 import java.util.ArrayList;
 
 /**
  * Created by Tatsunori on 19/04/2018.
  */
 public class Main {
-    public static void main(String[] args) throws IOException {
-        Bolsa ciel3 = new Bolsa("C:\\Users\\Tatsunori\\IdeaProjects\\trabalho1ia\\resources\\CIEL3.tsv");
-        Bolsa grnd3 = new Bolsa("C:\\Users\\Tatsunori\\IdeaProjects\\trabalho1ia\\resources\\GRND3.tsv");
-        Bolsa jslg3 = new Bolsa("C:\\Users\\Tatsunori\\IdeaProjects\\trabalho1ia\\resources\\JSLG3.tsv");
-        Bolsa lren3 = new Bolsa("C:\\Users\\Tatsunori\\IdeaProjects\\trabalho1ia\\resources\\LREN3.tsv");
-        Bolsa natu3 = new Bolsa("C:\\Users\\Tatsunori\\IdeaProjects\\trabalho1ia\\resources\\NATU3.tsv");
-        Bolsa prt4 = new Bolsa("C:\\Users\\Tatsunori\\IdeaProjects\\trabalho1ia\\resources\\PTR4.tsv");
-        Bolsa sbsp3 = new Bolsa("C:\\Users\\Tatsunori\\IdeaProjects\\trabalho1ia\\resources\\SBSP3.tsv");
-        Bolsa timp3 = new Bolsa("C:\\Users\\Tatsunori\\IdeaProjects\\trabalho1ia\\resources\\TIMP3.tsv");
-        Bolsa vivt3 = new Bolsa("C:\\Users\\Tatsunori\\IdeaProjects\\trabalho1ia\\resources\\VIVT3.tsv");
-        Bolsa wege3 = new Bolsa("C:\\Users\\Tatsunori\\IdeaProjects\\trabalho1ia\\resources\\WEGE3.tsv");
-
-        ArrayList<ArrayList> teste = ciel3.getBuffer();
-        for(ArrayList s: teste){
-            System.out.println(s);
-        }
+    public static void main(String[] args) throws IOException, ParseException {
+        Bolsa ciel3 = new Bolsa("resources\\CIEL3.tsv");
+        Bolsa grnd3 = new Bolsa("resources\\GRND3.tsv");
+        Bolsa jslg3 = new Bolsa("resources\\JSLG3.tsv");
+        Bolsa lren3 = new Bolsa("resources\\LREN3.tsv");
+        Bolsa natu3 = new Bolsa("resources\\NATU3.tsv");
+        Bolsa prt4 = new Bolsa( "resources\\PTR4.tsv");
+        Bolsa sbsp3 = new Bolsa("resources\\SBSP3.tsv");
+        Bolsa timp3 = new Bolsa("resources\\TIMP3.tsv");
+        Bolsa vivt3 = new Bolsa("resources\\VIVT3.tsv");
+        Bolsa wege3 = new Bolsa("resources\\WEGE3.tsv");
 
     }
 
--- src/model/Bolsa.java
+++ src/model/Bolsa.java
@@ -4,42 +4,40 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 
 /**
  * Created by Tatsunori on 13/05/2018.
  */
 public class Bolsa {
+    ArrayList<Cotacao> buffer = new ArrayList<Cotacao>();
+    Cotacao cotacao;
+    SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy");
 
-    //matriz, cada elemento de arraylist possui os 7 atributos do arquivo
-    ArrayList<ArrayList> buffer = new ArrayList<ArrayList>();
-
-
-    public Bolsa(String path) throws IOException {
+    public Bolsa(String path) throws IOException, ParseException {
         //
         BufferedReader br = new BufferedReader(new FileReader(path));
 
         while(br.ready()){
             String linha = br.readLine();
-            String linhasSeparadas[] = linha.split(" ");
-
             //arraylist para salvar cada linha separada
-            ArrayList aux = new ArrayList();
-            for(String s: linhasSeparadas){
-                aux.add(s);
-            }
+            String[] aux = linha.split(" ");
+
+
+            /*cotacao = new Cotacao();
+            cotacao.setData(formato.parse(aux[0]));
+            cotacao.setMin(Float.parseFloat(aux[1]));
+            cotacao.setMin(Float.parseFloat(aux[2]));
+            cotacao.setMax(Float.parseFloat(aux[3]));
+            cotacao.setVar(Float.parseFloat(aux[4]));
+            cotacao.setPvar(Float.parseFloat(aux[5]));
+            cotacao.setVol(Float.parseFloat(aux[6]));
 
             //linha completa, palavras salvas separadamente
-            buffer.add(aux);
-
+            buffer.add(cotacao);
+            */
         }
-    }
-
-    public ArrayList<ArrayList> getBuffer() {
-        return buffer;
-    }
-
-    public void setBuffer(ArrayList<ArrayList> buffer) {
-        this.buffer = buffer;
     }
 }
